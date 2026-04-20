5e63ab5e6e4c7a481464cb9230f5b1fc5c855d06
Drolf555 <derekrolfes21@gmail.com>
2026-04-19 21:56:34 -0400

    Remove alert and clean up theme toggle code

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-alert("JS is connected!"); // If you don't see this, the file name is wrong.
-
 document.addEventListener('DOMContentLoaded', () => {
     const themeBtn = document.getElementById('theme-toggle');
     if (themeBtn) {
